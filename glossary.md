8d1396361c2280afe2ad8aa5c0e3fbd8fbeb1790
Edward Barak <eine.fake.email.address@gmail.com>
2017-01-31 18:49:31 -0800

    Created python file and glossary.md

---
+++ glossary.md
@@ -0,0 +1,2 @@
+# Glossary
+**Option Chain:**  A listing of all the put and call option strike prices along with their premiums for a given maturity period. 
---
+++ mjoptions.py
@@ -0,0 +1,4 @@
+## Variables ## op = 'option'
+
+op_duration = {'min': 20, 'max': 40} # measured in days
+op_bwd = {'min:': -200, 'max': 200} # bwd = beta-weighted delta

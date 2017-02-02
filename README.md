3d73d51aae692f4d0594ea346e527f779fb9b8ff
Edward Barak <edward.barak@mysimplexity.com>
2017-02-02 00:45:49 -0800

    Added milestones for project. Started working towards the first milestone. Updated glossary.

--- README.md
+++ README.md
@@ -5,5 +5,11 @@
 3. If proven to work in any degree, attempt to test it using fake cash and live data
 4. If tests using fake cash and live data work, try with real cash and live data.
 5. If real cash and live data works, attempt to automate it.
+
+## Progress
+1. ( ) Write function that finds best written call strike range for WMT for the current day according to constraints.
+2. ( ) Output data to file.
+3. ( ) Simulate MJ strategy for 20 to 40 days.
+
 ## Notes
 * Reference materials can be found in references.md
--- mjoptions.py
+++ mjoptions.py
@@ -1,4 +1,41 @@
-## Variables ## op = 'option'
+import wallstreet as ws
+import yahoo_finance as yf
+import math
 
-op_duration = {'min': 20, 'max': 40} # measured in days
-op_bwd = {'min:': -200, 'max': 200} # bwd = beta-weighted delta
+## Variables ## op = 'option'
+op = {
+	'duration': {'min': 20, 'max': 40}, # measured in days
+	'bwd' = {'min:': -200, 'max': 200}, # bwd = beta-weighted delta
+	'd' = {'min': .08, 'max': .25}, # d = delta
+}
+
+## Functions
+
+def find_strike_range_call(ticker=ticker):
+	min_strike = {'strike': None, 'delta': None}
+	max_strike = {'strike': None, 'delta': None}
+
+	stock_price = float(yf.Share(ticker).get_price())
+	stock_price_ceil = math.ceil(stock_price)
+	stock = ws.Call(ticker, strike=stock_price_ceil)
+
+	# find max strike
+	for i in range(0,5):
+		option = ws.Call(ticker, strike=stock_price_ceil + i)
+
+		delta = option.delta()
+		if delta <= op['d']['max']:
+			max_strike = {'strike': option.strike, 'delta': delta}
+			break
+
+	# find min strike
+	for i in range(0,5):
+		option = ws.Call(ticker, strike=max_strike['strike'] + i)
+
+		delta = option.delta()
+		if delta <= op['d']['min']: 
+			break
+		else:
+			min_strike = {'strike': option.strike, 'delta': delta}
+
+	return min_strike, max_strike

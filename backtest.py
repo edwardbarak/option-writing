e60e5db00399c8060d9ca8098cebd9b391e5691d
Edward Barak <edward.barak@mysimplexity.com>
2017-02-16 10:33:20 -0800

    Further developed and cleaned up code to calculate greeks.

--- backtest.py
+++ backtest.py
@@ -1,43 +1,44 @@
 import pandas as pd
 import vollib as vl
-import dateutil
-from datetime import timedelta
+from dateutil import parse
+from time import strftime
 from wallstreet.blackandscholes import riskfree
 
-# dateutil.parser.parse('2016-01-01') 
-# dateutil.timedelta(end_date, start_date).days / float(365)
-#vl.b
+class option(object):
+	"""Object that caculates option greeks. Need current stock price, option's strike price, exiration month, day, and year, and sigma data to calculate implied volatility."""
+	def __init__(self, underlyingPrice, strike, expry_mm, expry_dd, expry_yyyy,  sigma_data):
+		super(option, self).__init__()
+		self.S = underlyingPrice
+		self.K = strike
+		self.t = calc_t(str(expry_mm) + '-' + str(expry_dd) + '-' + str(expry_yyyy))
+		self.r = calc_riskfree(self.t)
+        self.sigma = calc_sigma(sigma_data)
+
+    def calc_sigma(sigma_data):
+		# return sigma
+		pass
+
+    def calc_riskfree(t):
+        # get risk free rate. t is time to expiration in years.
+    	rate = riskfree()
+        return rate(t / float(252))
+
+    def calc_t():
+		# expry_date mm, dd, yy
+        expry_date = str(mm) + '-' + str(dd) + '-' + str(yyyy)
+        expry_date = p forarser.parse(expry_date)
+        current_date = parser.parse(strftime('%x'))
+        return expry_date - current_date
+
+
+### MAIN ###
 
 df = pd.read_csv('resources/datasets/SPY_2016.csv')
 
 flag = 'c'
-S = 201.019 # underlying asset price
-K = 170 	# strike price
-t = float(32) / float(252) # time to expiration in years, 252 because 252 is number of trading days in a year.
-r = float(.51) 		#risk free interest rate
-sigma = float(0.5) # annualized standard deviation, or volatility.
- 
-class option(object):
-	"""docstring for option"""
-	def __init__(self, underlyingPrice, strike, start_date, end_date, risk_free_rate, sigma_data):
-		super(option, self).__init__()
-		self.S = underlyingPrice
-		self.K = strike
-		self.start_date = start_date
-		self.end_date = end_date
-		self.r = risk_free_rate
-	def calc_sigma():
-		# calculate sigma via sigma_data df
-		# self.sigma = sigma
-		pass
-	def calc_t():
-		self.end_date = dateutil.parser.parse(self.end_date)
-		self.start_date = dateutil.parser.parse(self.start_date)
-		option_days = timedelta(end_date, start_date)
-		self.t = option_days / float(252)
-	def calc_greeks():
-		pass
-
-def get_r():
-	# get risk free rate
-	pass	
+S = 201.019                   # underlying asset price
+K = 170 	                  # strike price
+t = float(32) / float(252)    # time to expiration in years, 252 because 252 is number of trading days in a year.
+rate = riskfree()
+r = get_riskfree(t) 		  # risk free interest rate
+sigma = float(0.5)            # annualized standard deviation, or volatility.
--- glossary.md
+++ glossary.md
@@ -1,8 +1,12 @@
 # Glossary
-**Option Chain:**  A listing of all the put and call option strike prices along with their premiums for a given maturity period. 
+**Option Chain:**  A listing of all the put and call option strike prices along with their premiums for a given maturity period.
 
 **In-The-Money (ITM):** An option that can be exercised.
 
 **Out-Of-The-Money (OTM):** An option that can't be exercised.
 
 **At-The-Money (ATM):** An option who's value is the same as the stock itself.
+
+**vollib: numerical:** Approximate calculation.
+
+**vollib: analytical:** Precise calculation.
